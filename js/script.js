000e54913b9678f2ef3a1f92755b5e4bc38f24b8
Scott B. Sheffield <geekwise.scott.sheffield@gmail.com>
2016-03-15 14:27:19 -0700

    first-attempt-of-object-orienting
    began transitioning browser input based script to object oriented script

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
+//initial variables
 var input_box;
-var color_button;
+var button;
 var content_container;
 var content;
-var listPlaceholder;
+
+//links the input_box variable to the input tag in the html & sets the width to 50% of parent width
+function callInput(){
+    input_box = document.querySelector('#input');
+    input_box.style.width = '50%';
+}
+
+//links the button variable to the input tag in the html & assignees text to the button
+function callButton(){
+    button = document.querySelector('#button');
+    button.textContent = 'SUBMIT';
+}
+
+// links the content container variable to the unordered list tag in the html
+function callList(){
+    content_container = document.querySelector('#ul');
+}
+
+//when called, this function creates a list item & gets the syntax from the input
+function createItem(){
+    content = document.createElement('li');
+    content.textContent = input_box.value;
+}
+
+//calling this function will place content from the input bar to the top of the list
+function buttonActivated(){
+    callList().insertBefore(createItem(), callList().firstChild)
+}
 
 
 document.addEventListener('DOMContentLoaded',function(event){
 
-    //input_box = document.createElement('input');
-    input_box = document.querySelector('#input');
-    input_box.style.width = '50%';
-
-    color_button = document.querySelector('#button');
-    color_button.textContent = 'create color';
-
-    content_container = document.querySelector('#ul');
-
-    listPlaceholder = document.querySelector('#spclLI');
 
 
-    color_button.addEventListener('click',function(event){
-        content = document.createElement('li');
-        content.textContent = input_box.value;
-        //content.style.backgroundColor = input_box.value;
-        //content_container.appendChild(content);
-        content_container.insertBefore(content, content_container.firstChild);
 
-    });
 
-    
+
+
+    //button.addEventListener('click',function(event){
+    //    content_container.insertBefore(content, content_container.firstChild);
+    //});
+
+
 });

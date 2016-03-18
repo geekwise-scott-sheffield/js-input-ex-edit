e582d1ea0aef66b975a286a8c002cbef058a7d91
Scott B. Sheffield <geekwise.scott.sheffield@gmail.com>
2016-03-18 15:37:29 -0700

    todo-now-jQuery
    commented out old object oriented javascript and recreated application
    with jQuery

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
+//
+////independant: calls HTML input
+//var inputBox = document.querySelector("#input");
+////independant: calls HTML button
+//var button = document.querySelector("#button");
+////independant: calls HTML calls unordered list
+//var contentContainer = document.querySelector("#ul");
+////independant: variable; curently unassigned
+//var item;
+//
+////semi-independant: takes a parameter, makes it a li w/ content, nests in ul
+////foo = general parameter
+//function createListItem(foo){
+//    //makes parameter a list item
+//    foo = document.createElement("li");
+//    //list item is given syntax from input tag
+//    foo.textContent = inputBox.value;
+//    //list item is placed at the top of the unordered list
+//    contentContainer.insertBefore(foo, contentContainer.firstChild);
+//}
+//
 
-//independant: calls HTML input
-var inputBox = document.querySelector("#input");
-//independant: calls HTML button
-var button = document.querySelector("#button");
-//independant: calls HTML calls unordered list
-var contentContainer = document.querySelector("#ul");
-//independant: variable; curently unassigned
-var item;
 
-//semi-independant: takes a parameter, makes it a li w/ content, nests in ul
-//foo = general parameter
-function createListItem(foo){
-    //makes parameter a list item
-    foo = document.createElement("li");
-    //list item is given syntax from input tag
-    foo.textContent = inputBox.value;
-    //list item is placed at the top of the unordered list
-    contentContainer.insertBefore(foo, contentContainer.firstChild);
-}
-
+$(document).ready(function(){
+    $("#button").click(function(){
+        var content = $("#input").val();
+        var newItem = $(document.createElement('li'));
+        $(newItem).text(content);
+        $("#ul").prepend(newItem);
+    });
+});

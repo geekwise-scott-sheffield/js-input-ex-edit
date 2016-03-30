00a6a11539fbb6681d96cde946c6695bfd5167d8
Scott B. Sheffield <geekwise.scott.sheffield@gmail.com>
2016-03-29 18:19:57 -0700

    minor-commenting-out

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ app.controller('listCtrlr', [
     "$scope",
     function($scope){
 
-        $scope.test = 'halp';
+        //$scope.test = 'halp';
 
         $scope.content = [{
             title: "new items",
